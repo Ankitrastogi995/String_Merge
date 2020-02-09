bc56a6cbb83b77e492aee2da8f486d317df7f620
Ankit Rastogi <Ankitrastogi995>
2020-02-10 02:23:45 +0530

    added recursive function

--- src/StringMerge/Merge_String.java
+++ src/StringMerge/Merge_String.java
@@ -1,38 +1,33 @@
 package StringMerge;
+import java.util.*;
 
 
 public class Merge_String {
 
-	  
-    public static String merge(String s1, String s2, String s3) 
+	String result="";
+	int i=0;
+    public String merge(String s1, String s2, String s3,int len) 
     { 
-       
-        StringBuilder result = new StringBuilder(); 
-  
         
-        for (int i = 0; i < s1.length() || i < s2.length() || i < s3.length(); i++) { 
-  
-            
-            
-            if (i < s1.length()) 
-                result.append(s1.charAt(i)); 
-  
-             
-            if (i < s2.length()) 
-                result.append(s2.charAt(i));
-            if (i <s3.length())
-            	result.append(s3.charAt(i));
-        } 
-  
-        return result.toString(); 
+    	if(i<len) {
+    		result=result+s1.charAt(i)+s2.charAt(i)+s3.charAt(i);
+    		i++;
+    		merge(s1, s2, s3, len);
+    	}
+    	return result;
     } 
   
    
     public static void main(String[] args) 
     { 
-        String s1 = "geeks"; 
-        String s2 = "forgeeks"; 
-        String s3 = "Ankit";
-        System.out.println(merge(s1, s2,s3)); 
+        System.out.println("enter the String of equal length :");
+        Scanner sc =new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine(); 
+        String s3 = sc.nextLine();
+        
+        int len=s1.length();
+        Merge_String m=new Merge_String();
+        System.out.println(m.merge(s1, s2,s3,len)); 
     } 
 } 
